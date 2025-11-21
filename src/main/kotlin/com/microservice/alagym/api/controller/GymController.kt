45a4c689e518694185d62d159601898c3ba3cbd8
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2025-11-21 17:49:13 -0300

    feat: gym find for id

--- src/main/kotlin/com/microservice/alagym/api/controller/GymController.kt
+++ src/main/kotlin/com/microservice/alagym/api/controller/GymController.kt
@@ -3,6 +3,7 @@ package com.microservice.alagym.api.controller
 import org.springframework.web.bind.annotation.*
 import com.microservice.alagym.api.model.Gym
 import com.microservice.alagym.api.repository.GymRepository
+import org.springframework.http.ResponseEntity
 
 
 @RestController
@@ -18,5 +19,16 @@ class GymController(
     @PostMapping("/create")
     fun create(@RequestBody gym: Gym): Gym {
         return repo.save(gym)
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): ResponseEntity<Gym> {
+        val g = repo.findById(id)
+
+        return if (g.isPresent) {
+            ResponseEntity.ok(g.get())
+        } else {
+            ResponseEntity.notFound().build()
+        }
     }
 }
